bfc855da1e05ecc6d86029969e89084cfbdac2ee
kaja-iskierka <kajaiskierka@gmail.com>
2023-08-20 23:43:30 +0200

    JS i CSS updated

--- zadanie/main.js
+++ zadanie/main.js
@@ -1,69 +1,9 @@
 import './style.css'
-/*import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-
-/*const typesContainer = document.getElementById("types");
-
-fetch(https://api-eko-bazarek.azurewebsites.net/api/products/types)
-  .then(response => response.json())
-  .then(products => {
-    const productElement = document.createElement(types);
-
-      console.Consolelog(products);
-  })
-  .catch(error => {
-    console.error('Mamy blad',error);
-  }) 
-  */
-/*
- document.addEventListener('DOMContentLoaded', () => {
-   const typeButtons = document.querySelectorAll('#type-list button');
-   const categoryItems = document.querySelectorAll('#category-list li');
-
-   // Dodajemy nasłuchiwanie kliknięcia do przycisków typów
-   typeButtons.forEach(typeButton => {
-       typeButton.addEventListener('click', () => {
-           // Ukrywamy wszystkie kategorie
-           categoryItems.forEach(categoryItem => {
-               categoryItem.style.display = 'none';
-           });
-
-           // Pobieramy wartość typu z przycisku i pokazujemy odpowiadające kategorie
-           const typeId = typeButton.getAttribute('data-type-id');
-           const relatedCategories = document.querySelectorAll(`[data-category-type="${typeId}"]`);
-           relatedCategories.forEach(categoryItem => {
-               categoryItem.style.display = 'block';
-           });
-       });
-   });
-});
-*/
 
 const typesUrl = "https://api-eko-bazarek.azurewebsites.net/api/products/types";
 const categoriesUrl = "https://api-eko-bazarek.azurewebsites.net/api/products/categories";
-// var buttonGet = document.getElementById('DAIRY');
 var buttons = document.querySelectorAll('ul button');
+console.log(buttons)
 var typeList = document.getElementById('typeList');
 var categories = []
 getCategories().then(result => {
@@ -71,45 +11,60 @@ getCategories().then(result => {
   console.log(categories)
 })
 
-// function displayCategory(categories) {
-//   categories.forEach
-// }
 function displayCategory(categories) {
-  // categoryList.innerHTML = '';
+  var categoryList = document.querySelector('#categoryList')
+  console.log(categoryList)
+  categoryList.innerHTML = '';
+  categories.sort((categoryA, categoryB) => categoryA.name.localeCompare(categoryB.name));
   categories.forEach(category => {
-    const categoryElement = document.createElement('li');
-    categoryElement.textContent = `ID: ${category.id}, Nazwa: ${category.name}`;
-    typeList.appendChild(categoryElement);
+    var categoryElement = document.createElement('div');
+    categoryElement.setAttribute('class','eachProduct');
+    var categoryImage = document.createElement('img')
+    categoryImage.src = category.iconUrl
+    categoryImage.id = category.name
+    categoryElement.appendChild(categoryImage)
+    var categoryText = document.createElement('p')
+    categoryText.textContent = category.name
+    categoryElement.appendChild(categoryText)
+    categoryList.appendChild(categoryElement);
   });
 }
 
-
-// displayCategory(categories);
-
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const categoryId = button.getAttribute('data-category-id');
+    // const categoryId = button.getAttribute('data-category-id');
     const categoryName = button.getAttribute('data-category-name');
-    // filterById(categoryId);
-    console.log(`${categoryName}`);
-    // (`Kategoria ID: ${categoryId}, Nazwa: ${categoryName}`);
-
-    displayCategory(categories);
-
-    
+    console.log(button)
+    console.log(categories);
+    const filteredCategories = filterCategoriesById(categories, button.id)
+    displayCategory(filteredCategories);
   });
 });
 
-// function filterById(categoryId) {
-//   const filteredCategories = categories.filter(category => category.id === categoryId);
-//   addCategoryElements(filteredCategories);
-// }
-// displayCategory(categories);
-//   })
-// })
+
+function filterCategoriesById(categories, id) {
+  /* Chcemy:
+  1: Wziąć każdy z elemetów categories
+  2: Dla każdego z elementów categories sprawdzić czy jest zgodny z id
+  3: Zwrócić tylko te elementy które są tego typu
+   */
+  const filteredCat1 = categories.filter(c => c.type === id)
+
+
+  // var filteredCat2 = [];
+  // categories.forEach(c => {
+  //   if(c.type === id) {
+  //     filteredCat2.push(c)
+  //   }
+  // })
+  
+  return filteredCat1; 
+}
+
 
 async function getCategories() {
-  const response = await fetch(categoriesUrl)
+  try {
+  const response = await fetch(categoriesUrl);
 
   if (response.ok) {
     return response.json();
@@ -118,5 +73,11 @@ async function getCategories() {
     throw new Error('nie udalo sie pobrac kategorii. status:' + response.status);
   }
 }
+catch (error) {
+  const errormssg = document.getElementById('error-message');
+  errormssg.textContent = 'Nie udało się pobrać kategorri' + error.message;
+  }
+}
+
 
 
